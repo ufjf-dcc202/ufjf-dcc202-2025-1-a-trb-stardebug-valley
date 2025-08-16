1da0fe1dc3188d4764cf6c0f78c7fda391745adf
Enzo-Gabriell <enzogrledo@gmail.com>
2025-08-16 18:48:41 -0300

    pega corretamente as imagens

--- planta.js
+++ planta.js
@@ -7,25 +7,25 @@ class Planta {
 
         this.iconesEstagios = [];
         for (let i = 0; i <= totalEstagios; i++) {
-            this.iconesEstagios.push(`imagens/${nome}_estagio_${i}.png`);
+            this.iconesEstagios.push(`imagens/${nome}_Stage_${i}.png`);
         }
     }
 }
 
 class Rabanete extends Planta {
     constructor() {
-        super('rabanete', 1, 3, 50);
+        super('Radish', 1, 3, 50);
     }
 }
 
 class Cenoura extends Planta {
     constructor() {
-        super('cenoura', 2, 4, 80);
+        super('Carrot', 2, 4, 80);
     }
 }
 
 class Melao extends Planta {
     constructor() {
-        super('melao', 3, 5, 200);
+        super('Melon', 3, 5, 200);
     }
 }
