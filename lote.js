f6c41b9607705432b1eb974afedba32bb7b87fa0
Enzo-Gabriell <enzogrledo@gmail.com>
2025-08-17 16:30:52 -0300

    fix: lógica de colher e trocar de imagens

--- lote.js
+++ lote.js
@@ -19,6 +19,8 @@ export default class Lote {
     }
 
     arar() {
+        this.colher();
+
         if(this.estado === 'vazio') {
             this.estado = 'arado';
             this.atualizarVisual();
@@ -26,6 +28,8 @@ export default class Lote {
     }
 
     plantar(planta) {
+        this.colher();
+        
         if(this.estado === 'arado') {
             this.estado = 'plantado';
             
@@ -36,6 +40,8 @@ export default class Lote {
     }
 
     regar() {
+        this.colher();
+        
         if(this.estado === 'plantado') {
             this.regado = true;
 
@@ -44,20 +50,20 @@ export default class Lote {
     }
 
     avancarDia() {
-        if(this.planta) {
-            if(this.estagio >= this.planta.totalEstagios) {
-                this.estado = 'prontoParaColher';
-            }
-            else if(this.estado === 'plantado' && this.regado) {
+        if(this.planta && this.estado === 'plantado') {
+            
+            if(this.regado)
                 this.estagio++;
-                this.regado = false;
-            }
             else {
                 this.estado = 'vazio';
                 this.planta = null;
                 this.estagio = 0;
-                this.regado = false;
             }
+
+            this.regado = false;
+
+            if(this.estagio >= this.planta.totalEstagios - 1)
+                this.estado = 'prontoParaColher';
         }
 
         this.atualizarVisual();
@@ -79,6 +85,8 @@ export default class Lote {
     }
     
     cortarArvore() {
+        this.colher();
+        
         if(this.estado === 'arvore') {
             this.estado = 'vazio';
             this.atualizarVisual();
@@ -86,6 +94,8 @@ export default class Lote {
     }
 
     tirarPedra() {
+        this.colher();
+        
         if(this.estado === 'pedra') {
             this.estado = 'vazio';
             this.atualizarVisual();
@@ -93,27 +103,26 @@ export default class Lote {
     }
 
     atualizarVisual() {
+        this.elemento.innerHTML = '';
+        this.elemento.className = '';
 
-        // limpa antes de tudo
-        this.elemento.innerHTML = ''; 
-        this.elemento.className = ''; 
-        this.elemento.classList.add(this.estado);
+        if (this.estado === 'plantado' || this.estado === 'prontoParaColher') {
+            this.elemento.classList.add('arado'); 
+        } else {
+            this.elemento.classList.add(this.estado);
+        }
 
-        if (this.regado) {
+        if (this.regado && (this.estado === 'plantado' || this.estado === 'arado')) {
             this.elemento.classList.add('regado');
         }
 
-        if(this.estado === 'arvore')
+        if (this.estado === 'arvore') {
             this.elemento.innerHTML = `<img src="imagens/Arvore1.png" alt="arvore">`;
-        else if(this.estado === 'pedra') 
+        } else if (this.estado === 'pedra') {
             this.elemento.innerHTML = `<img src="imagens/Pedra.png" alt="pedra">`;
-        else if (this.planta && (this.estado === 'plantado' || this.estado === 'prontoParaColher')) {
-            
+        } else if (this.planta && (this.estado === 'plantado' || this.estado === 'prontoParaColher')) {
             const urlDaImagem = this.planta.iconesEstagios[this.estagio];
-
-            const imagemHTML = `<img src="${urlDaImagem}" alt="${this.planta.nome}">`;
-            
-            this.elemento.innerHTML = imagemHTML;
+            this.elemento.innerHTML = `<img src="${urlDaImagem}" alt="${this.planta.nome}">`;
         }
     }
 }
