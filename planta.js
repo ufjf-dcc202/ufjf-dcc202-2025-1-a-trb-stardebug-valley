075ff5b822c973d8f4ef89f289b1ede2e4fdb73c
Enzo-Gabriell <enzogrledo@gmail.com>
2025-08-17 16:05:06 -0300

    fix: estagios das plantas

--- planta.js
+++ planta.js
@@ -1,4 +1,4 @@
-export default class Planta {
+export class Planta {
     constructor(nome, tempoPorEstagio, totalEstagios, valorVenda) {
         this.nome = nome;
         this.tempoPorEstagio = tempoPorEstagio;
@@ -6,30 +6,28 @@ export default class Planta {
         this.valorVenda = valorVenda;
 
         this.iconesEstagios = [];
-        for (let i = 0; i <= totalEstagios; i++) {
+        // this.iconesEstagios.push(`imagens/${nome}_Seeds.png`);
+
+        for (let i = 1; i <= totalEstagios; i++) {
             this.iconesEstagios.push(`imagens/${nome}_Stage_${i}.png`);
         }
     }
-
-    getTotalEstagios() {
-        return this.totalEstagios;
-    }
 }
 
-class Rabanete extends Planta {
+export class Rabanete extends Planta {
     constructor() {
-        super('Radish', 1, 3, 50);
+        super('Radish', 1, 5, 50);
     }
 }
 
-class Cenoura extends Planta {
+export class Cenoura extends Planta {
     constructor() {
         super('Carrot', 2, 4, 80);
     }
 }
 
-class Melao extends Planta {
+export class Melao extends Planta {
     constructor() {
-        super('Melon', 3, 5, 200);
+        super('Melon', 3, 6, 200);
     }
 }
